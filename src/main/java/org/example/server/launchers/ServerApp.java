68b799ec2f7a455b6854fc8777dc2fe1dc6eebc6
Monarch93 <Monarch93@mail.ru>
2022-02-02 11:56:57 +0300

    homeworkJ2.8

---
+++ src/main/java/org/example/server/launchers/ServerApp.java
@@ -0,0 +1,9 @@
+package org.example.server.launchers;
+
+import org.example.server.MyServer;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
